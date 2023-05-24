a7fdac828186260555b642ae51e1db1d43bc43ea
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-05-24 23:23:34 +0300

    Completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const Timeout = setTimeout(() => {
+      if(timeRemaining>0){
+      setTimeRemaining(timeRemaining-1);
+      }
+      else{
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000);
 
+    return function(){
+      clearTimeout(Timeout)
+    }
+
+
+  })
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
